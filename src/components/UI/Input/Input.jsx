a081a6ff3505890285bffa12dc153a83bd193efe
Sri Vishnu <srivishp@gmail.com>
2023-08-19 15:44:29 +0530

    Using useImperativeHandle and forwardRef on input fields

---
+++ src/components/UI/Input/Input.jsx
@@ -0,0 +1,37 @@
+import React, { useRef, useImperativeHandle } from "react";
+import classes from "../Input/Input.module.css";
+
+// * With useImperativeHandle and forwardRef, we can expose functionality of a child componenet to a parent component
+// With that we can use a child compoenet inside/from the parent component
+
+const Input = React.forwardRef((props, ref) => {
+  const inputRef = useRef();
+
+  const activate = () => {
+    inputRef.current.focus();
+  };
+
+  useImperativeHandle(ref, () => {
+    return { focus: activate };
+  });
+
+  return (
+    <div
+      className={`${classes.control} ${
+        props.isValid === false ? classes.invalid : ""
+      }`}
+    >
+      <label htmlFor={props.id}>{props.label}</label>
+      <input
+        ref={inputRef}
+        type={props.type}
+        id={props.id}
+        value={props.value}
+        onChange={props.onChange}
+        onBlur={props.onBlur}
+      />
+    </div>
+  );
+});
+
+export default Input;
